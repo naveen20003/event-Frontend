dea1382b77825381a7f720d76f5473a4f22ea2c0
naveen20003 <navensaini9999@gmail.com>
2026-08-25 17:16:36 +0000

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -33,7 +33,7 @@ const formSchema = z.object({
   email: z.string().min(1, {
     message: "Email is required",
   }),
-  password: z.string().min(1, {
+  password: z.string().min(1, { 
     message: "Password is required"
   }),
 });
